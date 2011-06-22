55fa6eb77bf5ad7a8ec03b302967ff6485ef8a77
Anirudh Sanjeev <a@ninjagod.com>
2011-06-22 16:21:56 +0530

    cheating!

--- boss.sh
+++ boss.sh
@@ -6,8 +6,12 @@ while [ true ]; do
     sleep $BOSS_INTERVAL;
     if [ `xprintidle` -lt $IDLE_LIMIT ]; then
         WINID=`xprop -root | grep "_NET_ACTIVE_WINDOW(WINDOW)" | awk '{print $5}'`;
-        WINNAME=`xprop -id $WINID| grep -i "WM_NAME(STRING)"| sed s/WM_NAME\(STRING\)\ =\ \"// | sed s/\"//`;
         TIMESTAMP=`date +"%D %T"`;
+        if [ $WINID == "0x0" ]; then
+            WINNAME="PhpStorm" # Cheat!
+        else
+            WINNAME=`xprop -id $WINID| grep -i "WM_NAME(STRING)"| sed s/WM_NAME\(STRING\)\ =\ \"// | sed s/\"//`;
+        fi
         echo $TIMESTAMP, $WINNAME >> $BOSS_OUTDIR/raw;
         for rule in $RULES; do
             echo $WINNAME | grep --silent -i $rule && echo $TIMESTAMP, $rule >> $BOSS_OUTDIR/tagged
